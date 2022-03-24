f4207e3e5ba352520a16025c9874a61327edd9a1
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-24 12:38:14 -0300

    Ajustes y estilos al Header

---
+++ pages/nosotros.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const Nosotros = () => {
+    return (
+        <Layout>
+            <p>Nosotros</p>
+        </Layout>
+    )
+}
+
+export default Nosotros
